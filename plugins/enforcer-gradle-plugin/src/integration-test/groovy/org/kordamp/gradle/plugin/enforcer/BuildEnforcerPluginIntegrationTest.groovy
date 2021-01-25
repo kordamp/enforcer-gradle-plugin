532b5215380de910b0903037f76b5ebaafa1f954
Andres Almiray <aalmiray@gmail.com>
2021-01-25 13:04:34 +0100

    Update license headers

--- plugins/enforcer-gradle-plugin/src/integration-test/groovy/org/kordamp/gradle/plugin/enforcer/BuildEnforcerPluginIntegrationTest.groovy
+++ plugins/enforcer-gradle-plugin/src/integration-test/groovy/org/kordamp/gradle/plugin/enforcer/BuildEnforcerPluginIntegrationTest.groovy
@@ -1,7 +1,7 @@
 /*
  * SPDX-License-Identifier: Apache-2.0
  *
- * Copyright 2020 The author and/or original authors.
+ * Copyright 2020-2021 The author and/or original authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
